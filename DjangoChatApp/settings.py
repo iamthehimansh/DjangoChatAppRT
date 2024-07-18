3c294d66115ec32915b5b1803bbcba6ed988d31d
Himansh raj <iamthehimansh@gmail.com>
2024-07-18 21:12:22 +0530

    feat: Created chat app added Channels
    - Added 'channels' and 'chat' to the list of installed apps in settings.py
    - Configured ASGI application in settings.py
    - Added RedisChannelLayer configuration in settings.py
    - Created chat app

--- DjangoChatApp/settings.py
+++ DjangoChatApp/settings.py
@@ -37,7 +37,24 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    
+    'channels',
+    'chat',
 ]
+
+
+ASGI_APPLICATION = 'chatproject.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
