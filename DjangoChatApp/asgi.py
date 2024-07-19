401853195b15c5581ffdd599ff3e792cd8de9d5d
Himansh raj <iamthehimansh@gmail.com>
2024-07-19 12:37:44 +0530

    feat: Fix typo in ASGI configuration and added README.MD
    Recent user commits:
    - Add custom filter for formatting numbers
    - Add WebSocket support for chat functionality and used daphne for channels
    - Add WebSocket support for chat functionality
    - Created chat app and added Channels
    - Added 'channels' and 'chat' to the list of installed apps in settings.py
    - Configured ASGI application in settings.py
    - Added RedisChannelLayer configuration in settings.py

--- DjangoChatApp/asgi.py
+++ DjangoChatApp/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChatApp.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- chat/views.py
+++ chat/views.py
@@ -5,7 +5,7 @@ def index(request):
     rooms = ['Technology','Travel', 'Gaming']  # Add your rooms here
     room_data = {room: async_to_sync(ChatConsumer.get_room_user_count)(room) for room in rooms}
     # room_data["Technology"]=1600
-    room_data["Travel"]=887
+    room_data["Travel"]=3456
     room_data["Gaming"]=470
     return render(request, 'chat/index.html', {'rooms': room_data})
 
